e86705ceadc29e4175feb294db988dfd79fa7536
h2o <h2o_zs@163.com>
2021-08-05 17:15:26 +0800

    edit:question

--- part1/fed-e-task-01-01/code/code2.js
+++ part1/fed-e-task-01-01/code/code2.js
@@ -2,41 +2,41 @@ const fp = require('lodash/fp')
 // 数据
 // horsepower 马力, dollar_value 价格, in_stock 库存
 const cars = [{
-        name: 'Ferrari FF',
-        horsepower: 660,
-        dollar_value: 700000,
-        in_stock: true
-    },
-    {
-        name: 'Spyker C12 Zagato',
-        horsepower: 650,
-        dollar_value: 648000,
-        in_stock: false
-    },
-    {
-        name: 'Jaguar XKR-S',
-        horsepower: 550,
-        dollar_value: 132000,
-        in_stock: false
-    },
-    {
-        name: 'Audi R8',
-        horsepower: 625,
-        dollar_value: 114200,
-        in_stock: false
-    },
-    {
-        name: 'Aston Martin One-7',
-        horsepower: 750,
-        dollar_value: 1850000,
-        in_stock: true
-    },
-    {
-        name: 'Pagani Huayara',
-        horsepower: 700,
-        dollar_value: 1300000,
-        in_stock: true
-    }
+  name: 'Ferrari FF',
+  horsepower: 660,
+  dollar_value: 700000,
+  in_stock: true
+},
+{
+  name: 'Spyker C12 Zagato',
+  horsepower: 650,
+  dollar_value: 648000,
+  in_stock: false
+},
+{
+  name: 'Jaguar XKR-S',
+  horsepower: 550,
+  dollar_value: 132000,
+  in_stock: false
+},
+{
+  name: 'Audi R8',
+  horsepower: 625,
+  dollar_value: 114200,
+  in_stock: false
+},
+{
+  name: 'Aston Martin One-7',
+  horsepower: 750,
+  dollar_value: 1850000,
+  in_stock: true
+},
+{
+  name: 'Pagani Huayara',
+  horsepower: 700,
+  dollar_value: 1300000,
+  in_stock: true
+}
 ]
 
 /*
@@ -51,7 +51,7 @@ const cars = [{
 // 1.实现 isLastInStock 函数
 
 // 2.打印测试
-console.log( isLastInStock(cars) )  // 最终返回 true
+console.log(isLastInStock(cars))  // 最终返回 true
 
 /*
   练习2: 
@@ -61,7 +61,7 @@ console.log( isLastInStock(cars) )  // 最终返回 true
 // 1.实现 firstName 函数
 
 // 2.打印测试
-console.log( firstName(cars) )  // 最终返回 Ferrari FF (第一个 car 的 name) 
+console.log(firstName(cars))  // 最终返回 Ferrari FF (第一个 car 的 name) 
 
 
 /*
@@ -77,25 +77,43 @@ console.log( firstName(cars) )  // 最终返回 Ferrari FF (第一个 car 的 na
         return _average(dollar_values)
     }
 */
-  let _average = function (xs) {
-    return fp.reduce(fp.add, 0, xs) / xs.length
-  } // <- 无须改动
-  
-  // 1.实现 averageDollarValue 函数
-  
-  // 2.打印测试
-  console.log( averageDollarValue(cars) )  // 最终返回 
+let _average = function (xs) {
+  return fp.reduce(fp.add, 0, xs) / xs.length
+} // <- 无须改动
+
+// 1.实现 averageDollarValue 函数
+
+// 2.打印测试
+console.log(averageDollarValue(cars))  // 最终返回 
 
 /*
   练习4: 
   实现 sanitizeNames() 函数，要求使用 fp.flowRight()
-  把 返回一个下划线连接的小写字符串，
-  把数组中的 name 转换为这种形式： 例如：sanitizeNames(["Hello World"]) => ["hello_world"]
+  最终把 cars 当作参数, 应该得到下面结果
+    [
+      'ferrari_ff',
+      'spyker_c12_zagato',
+      'jaguar_xkr-s',
+      'audi_r8',
+      'aston_martin_one_7',
+      'pagani_huayara'
+    ]
+  即:
+  1) 生成名字数组
+  2) 名字全部小写
+  3) 空格等字符替换成下划线 (直接使用 _underscore 即可)
 */
 // 把非字母数字替换为下划线
 let _underscore = fp.replace(/\W+/g, '_') // <--无须改动
 
 // 1.实现 sanitizeNames 函数
-
+const cars = [
+  'ferrari_ff',
+  'spyker_c12_zagato',
+  'jaguar_xkr-s',
+  'audi_r8',
+  'aston_martin_one_7',
+  'pagani_huayara'
+]
 // 2.打印测试
-console.log( sanitizeNames(cars) )
+console.log(sanitizeNames(cars))
