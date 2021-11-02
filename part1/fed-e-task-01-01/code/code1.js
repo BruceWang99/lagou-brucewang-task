ded1e1dbb853683e7e324fcd2907d44085facac3
wangxiaoming8989 <wangxiaoming8989@sina.com>
2021-11-02 14:16:56 +0800

    task: 01-01-代码题1

--- part1/fed-e-task-01-01/code/code1.js
+++ part1/fed-e-task-01-01/code/code1.js
@@ -13,3 +13,21 @@
   }, 10)
 */
 
+// 改进1 (还是有地狱回调)
+Promise.resolve('hello ').then(a=>{
+	Promise.resolve('lagou ').then(b=>{
+		Promise.resolve('I lova U').then(c=>{
+			console.log(a + b + c);
+		})
+	})
+})
+
+// 改进2 链式调用
+Promise.resolve('hello ')
+.then(a=>{
+	let  b = 'lagou ';
+	return {a, b};
+}).then(({a, b})=>{
+	let c = 'I lova U'
+	console.log(a + b + c);
+})
